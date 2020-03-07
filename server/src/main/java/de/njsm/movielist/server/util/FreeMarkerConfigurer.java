6a667b0e5c36ead75b7d08fe1947d7dabfda8872
Jan Veen <jan.veen@posteo.de>
2020-03-07 13:28:07 +0100

    ML-4: Fix XSS vulnerability

---
+++ server/src/main/java/de/njsm/movielist/server/util/FreeMarkerConfigurer.java
@@ -0,0 +1,11 @@
+package de.njsm.movielist.server.util;
+
+import freemarker.template.Configuration;
+
+public class FreeMarkerConfigurer extends org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer {
+
+    @Override
+    protected Configuration newConfiguration() {
+        return new Configuration(Configuration.getVersion());
+    }
+}
